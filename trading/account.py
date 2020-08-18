3f2bcf7e4e0127519478da55574bc52f77533446
David Cohen <dovidcohen35@gmail.com>
2020-08-18 13:57:00 -0400

    privatized more account info

--- trading/account.py
+++ trading/account.py
@@ -3,7 +3,9 @@ import os
 
 
 auth_client = cbpro.AuthenticatedClient(key=os.getenv("key"), b64secret=os.getenv("b64secret"), passphrase=os.getenv("passphrase"))
-USD_account = (auth_client.get_account('256cc64c-a387-456e-9fe1-87ec139e4d2e'))
-ETH_account = (auth_client.get_account('9fbbf4ce-3dc6-4611-90d8-b475605caa8f'))
+usd_account=os.getenv("usd_account")
+USD_account = auth_client.get_account(usd_account)
+eth_account=os.getenv("eth_account")
+ETH_account = auth_client.get_account(eth_account)
 ETH_balance = str(ETH_account['available'])
 USD_balance = str(USD_account['available'])
