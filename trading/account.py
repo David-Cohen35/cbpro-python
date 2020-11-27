85719eeb4749e44572e4c826e293fd8d2cbd751c
David Cohen <dovidcohen35@gmail.com>
2020-11-26 20:56:13 -0500

    log change

--- trading/account.py
+++ trading/account.py
@@ -3,12 +3,12 @@ import os
 
 
 auth_client = cbpro.AuthenticatedClient(key=os.getenv("key"), b64secret=os.getenv("b64secret"), passphrase=os.getenv("passphrase"))
-usd_account=os.getenv("usd_account")
+usd_account = os.getenv("usd_account")
 USD_account = auth_client.get_account(usd_account)
 USD_balance = str(USD_account['available'])
-eth_account=os.getenv("eth_account")
+eth_account = os.getenv("eth_account")
 ETH_account = auth_client.get_account(eth_account)
 ETH_balance = str(ETH_account['available'])
-btc_account=os.getenv("btc_account")
+btc_account = os.getenv("btc_account")
 BTC_account = auth_client.get_account(btc_account)
 BTC_balance = str(BTC_account['available'])
